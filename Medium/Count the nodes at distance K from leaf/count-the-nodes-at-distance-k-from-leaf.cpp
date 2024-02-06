1c34c4d39eccad5c6ecd9a8eb8a9c6e947575395
siddhi-bajpai <115869460+siddhi-bajpai@users.noreply.github.com>
2024-02-06 16:48:33 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Count the nodes at distance K from leaf/count-the-nodes-at-distance-k-from-leaf.cpp
@@ -0,0 +1,175 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+//Node Structure
+/*struct NodeGiven a binary tree with n nodes and a non-negative integer k, the task is to count the number of special nodes. A node is considered special if there exists at least one leaf in its subtree such that the distance between the node and leaf is exactly k.
+{
+	int data;
+	Node *left, *right;
+};*/
+
+class Solution
+{
+    public:
+    //Function to return count of nodes at a given distance from leaf nodes.
+   int printKDistantfromLeaf(Node* root, int k)
+    {
+        unordered_map<Node *,Node *> m;
+        unordered_set<Node *> leaf;
+        unordered_set<Node *> ans;
+        
+        queue<Node *> q;
+        q.push(root);
+        m[root]=NULL;
+        
+        while(!q.empty())
+        {
+            Node * t=q.front();
+            q.pop();
+            
+            if(t->left==0 && t->right==0)
+                leaf.insert(t);
+                
+            if(t->left)
+            {
+                q.push(t->left);
+                m[t->left]=t;
+            }
+            if(t->right)
+            {
+                q.push(t->right);
+                m[t->right]=t;
+            }
+        }
+        
+        for(auto &val:leaf)
+        {
+            int cnt=0;
+            Node * t=val;
+            while(cnt!=k && t!=0)
+            {
+                cnt++;
+                t=m[t];
+            }
+            if(t!=0 && cnt==k)
+                ans.insert(t);
+        }
+        return ans.size();
+    }
+
+
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    string tc;
+    getline(cin, tc);
+    t=stoi(tc);
+    while(t--)
+    {
+        string s, ks;
+        getline(cin, s);
+        Node* root = buildTree(s);
+        getline(cin, ks);
+        int k=stoi(ks);
+        Solution obj;
+        cout<<obj.printKDistantfromLeaf(root, k)<<endl;
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
